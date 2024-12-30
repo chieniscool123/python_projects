45068ea823b5c40cd57044835c5b6b6da69ea9c5
Chien <chieniscool123@gmail.com>
2024-12-30 14:33:30 -0800

    Working with number in python

---
+++ python_number/hw2_10.py
@@ -0,0 +1,10 @@
+"""
+2-10. Favorite Number: Use a variable to represent your favorite number. Then,
+using that variable, create a message that reveals your favorite number. Print
+that message.
+
+"""
+
+FAV_NUM = 100
+
+print(f"{FAV_NUM} is my favorite number")
